610f6129b1aafdd73e8de926590e3a8b91de9df2
wolfstain <dahoyosr@unal.edu.co>
2018-05-03 03:46:23 +0000

    ldap

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 get 'auth/:provider/callback', to: 'sessions#create'
 delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
 
-
+resources :ldap
 resources :users
 root 'users#index'
 
