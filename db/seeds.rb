f0092759595927999478995a234dc7d264a954d4
wolfstain <dahoyosr@unal.edu.co>
2018-05-20 18:05:46 +0000

    seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(age: 12, name: 'Julian', gender: 'M', email: 'admin@admin.com', password:'administrator', picture:"http://bdfjade.com/data/out/142/6448580-profile-pictures.jpg")
-User.create!(age: 26, name: 'David', gender: 'M', email: 'dahoyosr@unal.edu.co', password:'123456789',picture:"http://kb4images.com/images/profile-pictures/38054870-profile-pictures.jpg")
-User.create!(age: 26, name: 'Maria', gender: 'F', email: 'maria@unal.edu.co', password:'123456789', picture: "https://www.fotkaplus.co.uk/wp-content/uploads/2016/04/carousel-profile-4.jpg")
+if User.find_by_email("admin@.com").nil?
+	User.create(age: 12, name: 'Julian', gender: 'M', email: 'admin@unal.edu.co', password:'administrator', picture:"http://bdfjade.com/data/out/142/6448580-profile-pictures.jpg")
+	User.create(age: 26, name: 'David', gender: 'M', email: 'dahoyosr@unal.edu.co', password:'123456789',picture:"http://kb4images.com/images/profile-pictures/38054870-profile-pictures.jpg")
+	User.create(age: 26, name: 'Maria', gender: 'F', email: 'maria@unal.edu.co', password:'123456789', picture: "https://www.fotkaplus.co.uk/wp-content/uploads/2016/04/carousel-profile-4.jpg")
+else
+   puts "Users already exists"
+end
 
